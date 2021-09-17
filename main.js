95f5889f456ecb32aa969d8b1d7a1f3d18a51af7
pcbenachio <pcbenachio@gmail.com>
2021-09-17 14:17:49 -0300

    EEC - prdImpression, prdClick, prdDetail

--- main.js
+++ main.js
@@ -1,1 +1,80 @@
-alert('Salve!')
+var prdImpression = document.querySelector('#prdImpression');
+var prdClick = document.querySelector('#prdClick');
+var prdDetail = document.querySelector('#prdDetail');
+
+var ecommerce = [prdImpression, prdClick, prdDetail]
+
+for (i=0; i < ecommerce.length; i++){
+    ecommerce[i].addEventListener(function(){
+        if (ecommerce[i] == prdImpression){
+            dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+            dataLayer.push({
+            'ecommerce': {
+                'currencyCode': 'EUR',                       // Local currency is optional.
+                'impressions': [
+                {
+                'name': 'Triblend Android T-Shirt',       // Name or ID is required.
+                'id': '12345',
+                'price': '15.25',
+                'brand': 'Google',
+                'category': 'Apparel',
+                'variant': 'Gray',
+                'list': 'Search Results',
+                'position': 1
+                },
+                {
+                'name': 'Donut Friday Scented T-Shirt',
+                'id': '67890',
+                'price': '33.75',
+                'brand': 'Google',
+                'category': 'Apparel',
+                'variant': 'Black',
+                'list': 'Search Results',
+                'position': 2
+                }]
+            }
+            });
+        }
+        else if (ecommerce[i] == prdClick){
+            dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+            dataLayer.push({
+                'event': 'productClick',
+                'ecommerce': {
+                'click': {
+                    'actionField': {'list': 'Search Results'},      // Optional list property.
+                    'products': [{
+                    'name': productObj.name,                      // Name or ID is required.
+                    'id': productObj.id,
+                    'price': productObj.price,
+                    'brand': productObj.brand,
+                    'category': productObj.cat,
+                    'variant': productObj.variant,
+                    'position': productObj.position
+                    }]
+                }
+                },
+                'eventCallback': function() {
+                document.location = productObj.url
+                }
+            });
+        }
+        else if (ecommerce == prdDetail){
+            dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+            dataLayer.push({
+            'ecommerce': {
+                'detail': {
+                'actionField': {'list': 'Apparel Gallery'},    // 'detail' actions have an optional list property.
+                'products': [{
+                    'name': 'Triblend Android T-Shirt',         // Name or ID is required.
+                    'id': '12345',
+                    'price': '15.25',
+                    'brand': 'Google',
+                    'category': 'Apparel',
+                    'variant': 'Gray'
+                }]
+                }
+            }
+            });
+        }
+    })
+}
